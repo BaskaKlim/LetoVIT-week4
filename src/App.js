adfa39b818f855b1ed5be22c773cdc6b0e625697
unknown <baska.klimekova@gmail.com>
2019-07-24 09:51:54 +0200

    Dynamicke TODOS v App.js. doteraz sme to mali v App.js natvrdo vvypisane

--- src/App.js
+++ src/App.js
@@ -1,29 +1,57 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Todo from './Todo';
 
-function App() {
-  return (
-    <div className="App p-3">
-      <Todo title="Nakupny zoznam" createdAt="23.09.1988" finished={false}>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Calamada</li>
-          <li class="list-group-item">Civapcici</li>
-          <li class="list-group-item">Cucoriedky </li>
-        </ul>
+class App extends Component {
+  state = {
+    todos: [
+      {
+        title: 'Precvic si JavaScript',
+        createdAt: '23.09.1988',
+        finished: false,
+        text: `<ul class="list-group list-group-flush">
+        <li class="list-group-item">Learn2Code kurz</li>
+        <li class="list-group-item">Vypracuj Petove cvicenia</li>
+        <li class="list-group-item">Domace ulohy z Leto v IT </li>
+      </ul> `
+      },
+      {
+        title: 'Nauc sa React',
+        createdAt: '17.07.2019',
+        finished: true,
+        text: `<ul class="list-group list-group-flush">
+        <li class="list-group-item">Calamada</li>
+        <li class="list-group-item">Civapcici</li>
+        <li class="list-group-item">Cucoriedky </li>
+      </ul> `
+      },
+      {
+        title: 'Zaverecny projekt',
+        createdAt: '28.08.2019',
+        finished: false,
+        text: `<ul class="list-group list-group-flush">
+        <li class="list-group-item">Learn2Code kurz</li>
+        <li class="list-group-item">Vypracuj Petove cvicenia</li>
+        <li class="list-group-item">Domace ulohy z Leto v IT </li>
+      </ul> `
+      }
 
-      </Todo>
-      <Todo title="Urob si ulohy z Reactu" createdAt="15.07.2019" finished={true}></Todo>
+    ]
+  }
 
 
-      <Todo title="Precvic si JavaScript" createdAt="19.07.2019" finished={false}>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Prezentacia z hodiny</li>
-          <li class="list-group-item">Domace ulohy</li>
-          <li class="list-group-item">Cvicenia z hodiny </li>
-        </ul>
-      </Todo>
-    </div>
-  );
+  render() {
+    const todos = this.state.todos;
+    return (
+      <div className="App p-3">
+        {todos.map(todoData => {
+          return (
+            <Todo todo={todoData} />
+          )
+        })}
+      </div>
+
+    );
+  }
 }
 
 export default App;
--- src/Todo.js
+++ src/Todo.js
@@ -6,15 +6,15 @@ class Todo extends Component {
 
     render() {
         // robim dynamicky prvok cize todo aby sa mi vedelo menit preto si ho vytvorim ako konstantu s atrobutmi
-        const { title, createdAt, children, finished } = this.props;
+        const { title, createdAt, text, finished } = this.props.todo;
 
 
         return (
 
-            <div className="todo mb-2" style= {{display: "inline-flex", margin: "1em"}}>
+            <div className="todo mb-2" style={{ display: "inline-flex", margin: "1em" }}>
                 {/* /pozor na zapis style toto berie ako objekt cize 2x kucerave zatvorky a este width bez uvodzoviek  + ak chcem pridat podmienku na ramcek border card tak si musim napisat if podmienku ku tejto 
                 class  a v podobe stringu preto tie backsticky  a ak card je podmienka: finish zbehneme mi ukon border -succes ak nie je splnena podmienka, nezbehne. Je to vraj uplne skratene IF syntakticky (google it)  */}
-                <div class={`card ${finished && ' border-success'}`} style={{ width: "18rem"}}>
+                <div class={`card ${finished && ' border-success'}`} style={{ width: "18rem" }}>
 
                     <div className="card-body">
                         <div class="card-header">
@@ -22,18 +22,18 @@ class Todo extends Component {
                                 {/* kedze si ho davama ako dynamicky prepisujem konkretny text Nakupny zoznam objektom co viem menit */}
                                 {title}</h5>
 
-                            <h6 className="card-subtitle mb-2 text-muted" style={{ margin: "1em" }}> 
+                            <h6 className="card-subtitle mb-2 text-muted" style={{ margin: "1em" }}>
                                 Vytvorene {createdAt}
                             </h6>
                         </div>
-                        {children ?
-                            <p className="card-text">
-                                {children}
-                            </p> : null 
+                        {text ?
+                            <div className="card-text" dangerouslySetInnerHTML={{ __html: text }} >
+
+                            </div> : null
 
                         }
-                        <br/>
-                        <Buttons todo={this.props} />
+                        <br />
+                        <Buttons todo={this.props.todo} />
 
                     </div >
 
