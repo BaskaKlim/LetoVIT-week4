8117d5a61b0a62ee2ef3ac07defd92f6d9723d1a
unknown <baska.klimekova@gmail.com>
2019-08-20 09:56:10 +0200

    url change in axios from firebase to localhost

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 
 import axios from 'axios';
-
+// @TODO custom api url here
 const instance = axios.create({
+// we change our server from firebase to local  baseURL: 'https://todo-app-course.firebaseio.com'
     baseURL: 'http://localhost:8080',
 }
 );
